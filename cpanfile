5b6e9e04526e3da268545df35b8380fc9d031cb8
Michael Zhao <michaelzhao314@gmail.com>
2023-08-16 23:06:57 -0500

    Added dancing man

---
+++ cpanfile
@@ -0,0 +1,2 @@
+requires 'Term::ANSIScreen';
+requires 'Time::HiRes';
---
+++ hello.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use Term::ANSIScreen qw(cls);
+use Time::HiRes qw(usleep);
+
+my $clear_screen = cls();
+
+sub chooseNum
+{
+    my $first = $_[0];
+    my $second = $_[1];
+    my $choice = int(rand(2));
+
+    if ($choice eq 0) {
+        return $first;
+    } else {
+        return $second;
+    }
+}
+
+while (1) {
+    $left = choose("\\", "_");
+    $middle = choose("o", "O");
+    $right = choose("/", "_");
+    $body = choose("(", ")");
+    $leftLeg = choose("/", "⅃");
+    $rightLeg = choose("\\", "L");
+
+    print "\n${left}${middle}${right}\n ${body}\n${leftLeg} ${rightLeg}\n";
+
+    usleep(200000);
+
+    print $clear_screen;
+}
